dfeb8c7292fe9f8719ecc728c5c7af2869e2c217
Ryan Crowley <ryan@memreel.io>
2024-01-05 14:13:07 -0800

    adding unit failing unit tests for rent roll

--- test/models/rent_roll_test.rb
+++ test/models/rent_roll_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class RentRollTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  TODAY               = Date.today
+  YESTERDAY           = TODAY - 1.day
+  TOMORROW            = TODAY + 1.day
+  DAY_AFTER_TOMORROW  = TOMORROW + 1.day
+
+  test "the unit is available with no move in or move out date set" do 
+    roll = RentRoll.new
+    assert roll.available_on(TODAY)
+  end
+
+  test "the unit is available for date after move_out date" do 
+    roll = RentRoll.new
+    roll.move_in = YESTERDAY
+    roll.move_out = TOMORROW
+    assert roll.available_on(DAY_AFTER_TOMORROW)
+  end
+
+  test "the unit is not available for date within range of move_in/move_out" do 
+    roll = RentRoll.new
+    roll.move_in = YESTERDAY
+    roll.move_out = TOMORROW
+    assert_not roll.available_on(TODAY)
+  end
+
+  test "the unit is not available for date after move_in without move_out date set" do 
+    roll = RentRoll.new
+    roll.move_in = YESTERDAY
+    assert_not roll.available_on(TODAY)
+  end
 end
